70d50b8c8060cc8f004460cf9b1252482a2d038b
MorganeFi <morgane.fierville@gmail.com>
2026-06-08 13:43:47 +0200

    modif scispy to sparty

--- src/scispy/io/__init__.py
+++
@@ -1,7 +0,0 @@
-from .basic import load_cosmx, load_merscope, load_xenium
-
-__all__ = [
-    "load_merscope",
-    "load_xenium",
-    "load_cosmx",
-]
--- src/scispy/pp/__init__.py
+++
@@ -1,7 +0,0 @@
-from .basic import run_scanpy, scvi_annotate, sync_shape
-
-__all__ = [
-    "run_scanpy",
-    "scvi_annotate",
-    "sync_shape",
-]
